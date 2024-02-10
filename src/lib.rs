308e52859f6f667a2a125827a9794c880d1554a5
Bryanp <dasassygoat@coderbox.co>
2024-02-10 01:47:32 -0600

    custom error message

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+use std::fmt::format;
+
 pub fn add_two(a:i32) -> i32{
     a + 3
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {name}!") // no semi colon
 }
 
 #[cfg(test)]
@@ -9,5 +15,11 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
     }
 }
