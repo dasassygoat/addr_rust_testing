6d8089a9637c6b20e57edd0a85157a968134a8ec
dasassygoat <dasassygoat@coderbox.co>
2024-02-19 22:56:15 -0600

    clean up old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 
 
-// pub fn add_two(a: i32) -> i32 {
-//     a + 3
-// }
-
 pub fn add_two(a: i32) -> i32 {
     internal_adder(a, 2)
 }
@@ -12,48 +8,13 @@ fn internal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
 
-pub fn greeting(_name: &str) -> String {
-    format!("Hello") // no semi colon
-}
-pub struct Guess {
-    value:i32,
-}
-impl Guess{
-    pub fn new(value:i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}",
-            value);
-        }
-
-        Guess {value}
-    }
-}
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    //#[test]
-    //fn it_adds_two() {
-    //  assert_eq!(4, add_two(2));
-    //}
 
     #[test]
     fn internal(){
         assert_eq!(4, internal_adder(2, 2));
     }
 
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{result}`"
-        );
-    }
-
-    #[test]
-    #[should_panic]
-    fn greater_than_100(){
-        Guess::new(23);
-    }
 }
